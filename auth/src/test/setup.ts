a93c878bc64eddb95869dfa076eb230459ddca44
ftomi <tomifarkas@yahoo.com>
2020-09-21 13:11:14 +0200

    Signup test

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,21 +1,23 @@
-import { MongoMemoryServer } from "mongodb-memory-server";
-import mongoose from "mongoose";
-import { app } from "../app";
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import mongoose from 'mongoose';
+import { app } from '../app';
 
 let mongo: any;
-
 beforeAll(async () => {
+  process.env.JWT_KEY = 'asdfasdf';
+
   mongo = new MongoMemoryServer();
   const mongoUri = await mongo.getUri();
 
   await mongoose.connect(mongoUri, {
     useNewUrlParser: true,
-    useUnifiedTopology: true,
+    useUnifiedTopology: true
   });
 });
 
 beforeEach(async () => {
   const collections = await mongoose.connection.db.collections();
+
   for (let collection of collections) {
     await collection.deleteMany({});
   }
