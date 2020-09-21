99bf5f6d755a5a3ab559cf6a3639776ea37bf697
ftomi <tomifarkas@yahoo.com>
2020-09-21 13:47:04 +0200

    Currentuser test and global signin for tests

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,18 +1,28 @@
-import { MongoMemoryServer } from 'mongodb-memory-server';
-import mongoose from 'mongoose';
-import { app } from '../app';
+// import { request } from "express";
+import { MongoMemoryServer } from "mongodb-memory-server";
+import mongoose from "mongoose";
+import request from "supertest";
+import { app } from "../app";
 
-let mongo: any;
+declare global {
+  namespace NodeJS {
+    interface Global {
+      signin(): Promise<string[]>;
+    }
+  }
+}
+
+let mongod: any;
 beforeAll(async () => {
-  process.env.JWT_KEY = 'asdfasdf';
-  process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+  process.env.JWT_KEY = "asdfasdf";
+  process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
-  mongo = new MongoMemoryServer();
-  const mongoUri = await mongo.getUri();
+  mongod = new MongoMemoryServer();
+  const mongoUri = await mongod.getUri();
 
   await mongoose.connect(mongoUri, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
   });
 });
 
@@ -25,6 +35,19 @@ beforeEach(async () => {
 });
 
 afterAll(async () => {
-  await mongo.stop();
+  await mongod.stop();
   await mongoose.connection.close();
 });
+
+global.signin = async () => {
+  const email = "test@test.com";
+  const password = "password";
+
+  const response = await request(app)
+    .post("/api/users/signup")
+    .send({ email, password })
+    .expect(201);
+
+  const cookie = response.get("Set-Cookie");
+  return cookie;
+};
